c013798af111854558b58f7879b546b0ba562680
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 16:58:12 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,4 +1,5 @@
 def select_books_titles_and_years_in_first_series_order_by_year
+  
   <<-SQL 
     SELECT title, year FROM books WHERE series_id = 1 
     ORDER BY year 
@@ -7,6 +8,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
+  
   <<-SQL 
     SELECT name, motto FROM characters ORDER BY LENGTH(motto)
     DESC LIMIT 1 
@@ -16,6 +18,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
+  
   <<-SQL 
     SELECT species, COUNT(*) FROM characters GROUP BY species
     ORDER BY COUNT(species)
@@ -25,6 +28,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
+  
   <<-SQL 
     SELECT authors.name, subgenres.name FROM authors JOIN series 
     ON series.author_id = authors.id 
@@ -35,10 +39,20 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
+  
   <<-SQL 
-  SELECT series.title 
+    SELECT series.title FROM series JOIN books 
+    ON books.series_id = series. id 
+    JOIN character_books ON character_books.book_id = books.id 
+    JOIN characters ON character_books.character_id = characters.id 
+    WHERE characters.species = 'human' 
+    GROUP BY series.title ORDER BY COUNT (*) 
+    DESC LIMIT 1 
+    SQL
+  
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  <<-SQL 
+    
 end
