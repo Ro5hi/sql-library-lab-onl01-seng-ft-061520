994a66d67c7465337cb1863a94a64c6d3c568b07
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 16:52:57 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,9 +29,9 @@ def select_name_and_series_subgenres_of_authors
     SELECT authors.name, subgenres.name FROM authors JOIN series 
     ON series.author_id = authors.id 
     JOIN subgenres 
-    ON series.subgenres_id = 
+    ON series.subgenres_id = subgenres.id 
+    SQL
     
-  
 end
 
 def select_series_title_with_most_human_characters
