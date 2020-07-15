53234de5a9ced837c2ef33588cf976b11ce2bdd1
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 16:50:22 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -17,7 +17,8 @@ end
 
 def select_value_and_count_of_most_prolific_species
   <<-SQL 
-    SELECT species, COUNT(*) FROM characters GROUP BY 
+    SELECT species, COUNT(*) FROM characters GROUP BY species
+    
 end
 
 def select_name_and_series_subgenres_of_authors
