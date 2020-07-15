f267153946c336e619949a2438cd275066449083
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-15 16:52:14 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -27,7 +27,8 @@ end
 def select_name_and_series_subgenres_of_authors
   <<-SQL 
     SELECT authors.name, subgenres.name FROM authors JOIN series 
-    ON series.author_id = authors_id
+    ON series.author_id = authors.id 
+    
     
   
 end
